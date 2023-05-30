af56097174016917cfef3fdecba164eeeac28496
jckyagno <133424867+jckyagno@users.noreply.github.com>
2023-05-30 11:18:16 -1000

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction() {                                     //Creating a variable
+    var A = "You are";                                      //Defining variable A
+    var B = " super awesome!";                              //Defining variable B
+    A += B;                                                 //Concatenating variable A and B
+    document.getElementById("functionId").innerHTML = A;    //Assigning ID, referencing varible A
+}
+
